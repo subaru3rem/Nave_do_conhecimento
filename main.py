9500ca2c59cc3995f7bdcce0e99a0ecc44369f44
subaru3rem <cainancosta6@gmail.com>
2022-10-18 13:45:31 -0300

    nova pagina para coletar informações do cadastro

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ mydb = mysql.connector.connect(
     database = 'serve_nave'
 )
 cursor = mydb.cursor()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -29,6 +27,12 @@ def user():
     return render_template("area do aluno.html")
 def Login(login):
     return render_template("log_user.html", account=login)
+@app.route("/user/j")
+def cad():
+    return render_template("finalizar_cad.html")
+@app.route("/user/j", methods = ["GET", "POST"])
+def finalizar_cad():
+    pass
 @app.route('/user', methods =["GET", "POST"])
 def validacion():
     if request.method == "POST":
@@ -48,7 +52,7 @@ def validacion():
                     if cad_senha == confirmação:
                         cursor.execute(f"insert into usuarios values ('{cad}', '{cad_senha}')")
                         mydb.commit()
-                        return Login(cad)
+                        return cad()
                     else: 
                         flash("senhas_distintas")
                         return user()

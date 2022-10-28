9358727cc66d4432b8f85a4a0fc23771674d180c
Cainã Costa <113543504+subaru3rem@users.noreply.github.com>
2022-10-28 17:23:11 -0300

    main sanitizado

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fileinput import filename
 from nturl2path import pathname2url
 from flask import *
 import mysql.connector
+import re
 
 mydb = mysql.connector.connect(
     host='localhost',
@@ -30,14 +31,33 @@ def Login(login):
 @app.route("/user/#")
 def Cad(cad, cad_senha):
     return render_template("finalizar_cad.html", login=cad, senha=cad_senha)
+def sanitize(dict):
+    chave = dict[0]
+    string = dict[1]
+    if chave == "email":
+        string = re.sub("[^\w@.-_]", "", string)
+        return string
+    elif chave == "cll":
+        string = re.sub("[\D ]", "", string)
+        if len(string) == 11:
+            string = "0"+string
+            return string
+        elif len(string) == 12:
+            return string
+    else:
+        string = re.sub("[^\w ]", "", string)
+        return string
 @app.route("/user/#", methods = ["GET", "POST"])
 def finalizar_cad():
     login, senha, nome, nome_s = request.form.get("user"),request.form.get("user_senha"),request.form.get("nome"), request.form.get("nome_s")
     sexo, email, data_n, cpf = request.form.get("sexo"), request.form.get("email"), request.form.get("data_n"), request.form.get("cpf")
     cell, uf, cidade, cor = request.form.get("celular"), request.form.get("uf"), request.form.get("cidade"), request.form.get("cor")
-    cursor.execute(f"""insert into usuarios values('{login}', '{senha}');""")
-    cursor.execute(f'''insert into info_user values('{login}', '{nome}', '{nome_s}', '{sexo}', '{email}', '{data_n}'
-    , '{cpf}', '{cell}', '{uf}', '{cidade}', '{cor}')''')
+    itens = {'login':login, 'senha':senha, 'nome':nome, 'nome_s':nome_s, 'sexo':sexo, 'email':email, 'data_n': data_n, 'cpf':cpf, 'cell': cell, 'uf':uf, 'cidade':cidade, 'cor': cor}
+    clean = []
+    for i in itens.items():clean.append(sanitize(i))
+    cursor.execute(f"""insert into usuarios values('{clean[0]}', '{clean[1]}');""")
+    cursor.execute(f'''insert into info_user values('{clean[0]}', '{clean[2]}', '{clean[3]}', '{clean[4]}', '{clean[5]}', '{clean[6]}'
+    , '{clean[7]}', '{clean[8]}', '{clean[9]}', '{clean[10]}', '{clean[11]}')''')
     mydb.commit()
     return Login(login)
 @app.route('/user', methods =["GET", "POST"])
